807e67bdc8bd77256a9de86f25894c42bd61ee16
Deyvison Penha <deyvisonpenha1@gmail.com>
2023-05-08 01:42:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const calculateSalesTaxAndTotal = require("./utils/taxCalculator");
 
 const { input1, input2, input3 } = items;
 
-const t = calculateSalesTaxAndTotal(input3);
+calculateSalesTaxAndTotal(input3);
 console.log("-------");
-console.log(t);
-// calculateSalesTaxAndTotal(input2);
-// console.log("-------");
-// calculateSalesTaxAndTotal(input3);
+calculateSalesTaxAndTotal(input2);
+console.log("-------");
+calculateSalesTaxAndTotal(input3);
